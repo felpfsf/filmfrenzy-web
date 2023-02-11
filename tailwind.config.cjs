f4ea69e9108f47af984dc51a312971a95caebcb4
Felipe <feferreira.sf@gmail.com>
2023-02-11 19:36:14 -0300

    style: linear gradient custom

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage:{
+        posterGradient: 'linear-gradient(to top, #111 10%, transparent 90%)',
+        contentPosterGradient: 'linear-gradient(to top, #111 30%, transparent 70%)',
+      }
+    }
   },
   plugins: []
 }
