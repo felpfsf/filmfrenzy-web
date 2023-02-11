3ef97c454c0b2ca5aad6360583df59bd8ef96de5
Felipe <feferreira.sf@gmail.com>
2023-02-11 18:26:21 -0300

    feat: navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,111 @@
+import { useState } from 'react'
+import { RxCross1, RxHamburgerMenu } from 'react-icons/rx'
+import { HiOutlineMagnifyingGlass } from 'react-icons/hi2'
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(true)
+
+  const menuItems = [
+    { to: '/movies', text: 'Movies' },
+    { to: '/shows', text: 'Tv Shows' },
+    { to: '/upcomming', text: 'Up Comming' }
+  ]
+
+  const toggleNavMenu = () => {
+    setIsNavbarOpen(prev => !prev)
+  }
+
+  return (
+    <div className='fixed m-auto flex w-full items-center justify-between bg-gradient-to-r from-[#111]/30 to-transparent/5 py-4 px-8'>
+      <h1 className='text-4xl font-black tracking-widest'>FILMFRENZY</h1>
+      <button
+        className='z-10 block h-8 w-8 xl:hidden'
+        onClick={toggleNavMenu}
+        aria-label='Toggle navigation menu'
+        aria-expanded={isNavbarOpen}>
+        {isNavbarOpen ? (
+          <RxCross1 className='h-full w-full' />
+        ) : (
+          <RxHamburgerMenu className='h-full w-full' />
+        )}
+      </button>
+      <nav className='hidden flex-1 justify-between xl:flex'>
+        <div className='ml-12 flex items-center gap-8'>
+          {menuItems.map(item => (
+            <Link
+              className='border-b-2 border-b-transparent font-bold hover:border-b-neutral-200'
+              key={item.to}
+              to={item.to}
+              aria-current='page'>
+              {item.text}
+            </Link>
+          ))}
+          <div className='relative'>
+            <input
+              className='border-b border-b-neutral-200 bg-transparent px-2 placeholder-neutral-200 placeholder:italic'
+              type='text'
+              aria-label='Search movies and tv shows'
+              aria-placeholder='Search for Movies and TV Shows'
+              placeholder='Search Movies and Shows'
+            />
+            <HiOutlineMagnifyingGlass className='absolute top-0 right-2' />
+          </div>
+        </div>
+        <div className='flex gap-2'>
+          <button
+            className='bg-neutral-200 px-4 py-2 duration-500 hover:ring-1'
+            aria-label='Sign In button'>
+            Sign In
+          </button>
+          <button
+            className='bg-neutral-200 px-4 py-2 duration-500 hover:ring-1'
+            aria-label='Sign Up button'>
+            Sign Up
+          </button>
+        </div>
+      </nav>
+      {/* mobile menu */}
+      <nav
+        className={
+          isNavbarOpen
+            ? 'fixed top-0 right-0 flex h-screen w-full flex-col items-center gap-y-10 bg-red-500 duration-200 ease-in-out'
+            : 'fixed top-0 -right-full flex h-screen w-full flex-col items-center gap-y-10 bg-red-500 duration-200 ease-in-out'
+        }>
+        <div className='flex flex-col items-center gap-8 pt-24'>
+          {menuItems.map(item => (
+            <Link
+              className='border-b-2 border-b-transparent font-bold hover:border-b-neutral-200'
+              key={item.to}
+              to={item.to}
+              aria-current='page'>
+              {item.text}
+            </Link>
+          ))}
+          <div className='relative'>
+            <input
+              className='border-b border-b-neutral-200 bg-transparent px-4 placeholder-neutral-200 placeholder:italic'
+              type='text'
+              aria-label='Search movies and tv shows'
+              aria-placeholder='Search for Movies and TV Shows'
+              placeholder='Search Movies and Shows'
+            />
+            <HiOutlineMagnifyingGlass className='absolute top-1 right-1' />
+          </div>
+        </div>
+        <div className='flex gap-2'>
+          <button
+            className='bg-neutral-200 px-4 py-2 duration-500 hover:ring-1'
+            aria-label='Sign In button'>
+            Sign In
+          </button>
+          <button
+            className='bg-neutral-200 px-4 py-2 duration-500 hover:ring-1'
+            aria-label='Sign Up button'>
+            Sign Up
+          </button>
+        </div>
+      </nav>
+    </div>
+  )
+}
