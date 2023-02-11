c55f98d817830d816bc6df8f38ba1ea88c1c9f7a
Felipe <feferreira.sf@gmail.com>
2023-02-11 20:15:44 -0300

    feat: export env

---
+++ src/utils/env.ts
@@ -0,0 +1,3 @@
+export const TMDB_API_KEY = import.meta.env.VITE_TMDB_KEY
+export const TMDB_BASE_URL = import.meta.env.VITE_BASE_URL
+export const TMDB_BACKDROP_POSTER = import.meta.env.VITE_POSTER_URL
