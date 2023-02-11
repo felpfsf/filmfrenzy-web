f24897c2866a4d0f0b8431d499f25d89f1519906
Felipe <feferreira.sf@gmail.com>
2023-02-11 17:06:59 -0300

    feat: home routes config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from 'react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import queryClient from './lib/queryClient'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import './styles/index.css'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
+  </React.StrictMode>
 )
