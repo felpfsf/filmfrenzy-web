e81a5f980395b47b387be5a45dcb9f4fa4ed2e17
Felipe <feferreira.sf@gmail.com>
2023-02-16 17:48:54 -0300

    chore: tailwind text-fill plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,18 @@ module.exports = {
       backgroundImage:{
         posterGradient: 'linear-gradient(to top, #111 10%, transparent 90%)',
         contentPosterGradient: 'linear-gradient(to top, #111 30%, transparent 70%)',
+      },
+      fontFamily:{
+        logo: ['Bebas Neue', 'cursive'],
+        main: ['Inter', 'sans-serif'],
+      },
+      colors:{
+        button: '#F40009',
+        button_hover: '#A20025',
       }
     }
   },
-  plugins: []
+  plugins: [
+    require('tailwindcss-text-fill')
+  ]
 }
