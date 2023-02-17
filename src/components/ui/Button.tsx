8d63de5e7faa50d61b557731ebff3077ed87a0ab
Felipe <feferreira.sf@gmail.com>
2023-02-17 15:47:58 -0300

    style: change colors

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 export const Button = ({ label, handleClick }: ButtonProps) => {
   return (
     <button
-      className='rounded-md bg-button px-4 py-2 font-semibold ring-[#FFB500] duration-500 hover:bg-button_hover hover:ring-1'
+      className='rounded-md bg-primary px-4 py-2 font-semibold ring-[#FFB500] duration-500 hover:bg-secondary hover:ring-1 active:bg-accent'
       onClick={handleClick}
       aria-label={`${label} button`}
       >
