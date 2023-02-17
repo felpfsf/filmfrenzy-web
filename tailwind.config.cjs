90d6fa638f1e15acf2de7a2f33a533ec83b98e9f
Felipe <feferreira.sf@gmail.com>
2023-02-17 15:44:11 -0300

    style: added color pattern

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,21 +3,23 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      backgroundImage:{
+      backgroundImage: {
         posterGradient: 'linear-gradient(to top, #111 10%, transparent 90%)',
-        contentPosterGradient: 'linear-gradient(to top, #111 30%, transparent 70%)',
+        contentPosterGradient:
+          'linear-gradient(to top, #111 30%, transparent 70%)'
       },
-      fontFamily:{
+      fontFamily: {
         logo: ['Bebas Neue', 'cursive'],
-        main: ['Inter', 'sans-serif'],
+        main: ['Inter', 'sans-serif']
       },
-      colors:{
-        button: '#F40009',
-        button_hover: '#A20025',
+      colors: {
+        primary: '#F40009',
+        secondary: '#A20025',
+        accent: '#800000',
+        paragraph:'#FAEBD7',
+        background: '#171717'
       }
     }
   },
-  plugins: [
-    require('tailwindcss-text-fill')
-  ]
+  plugins: [require('tailwindcss-text-fill')]
 }
