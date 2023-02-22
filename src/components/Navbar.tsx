b1ba6f5d26465fec0f0be244e16ad5f31e1694aa
Felipe <feferreira.sf@gmail.com>
2023-02-22 15:09:27 -0300

    feat: using searchbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import { RxCross1, RxHamburgerMenu } from 'react-icons/rx'
-import { HiOutlineMagnifyingGlass } from 'react-icons/hi2'
-import { Link, NavLink } from 'react-router-dom'
-import { Button } from './ui/Button'
+import { useState } from "react";
+import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
+import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import { Link, NavLink } from "react-router-dom";
+import { Button } from "./ui/Button";
+import { Searchbar } from "./Searchbar";
 
 export const Navbar = () => {
-  const [isNavbarOpen, setIsNavbarOpen] = useState(false)
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
   const menuItems = [
-    { to: '/movies', text: 'Movies' },
-    { to: '/tvshows', text: 'Tv Shows' },
-    { to: '/upcomming', text: 'Up Comming' }
-  ]
+    { to: "/movies", text: "Movies" },
+    { to: "/tvshows", text: "Tv Shows" },
+    { to: "/upcomming", text: "Up Comming" },
+  ];
 
   const toggleNavMenu = () => {
-    setIsNavbarOpen(prev => !prev)
-  }
+    setIsNavbarOpen((prev) => !prev);
+  };
 
   return (
     <div className='fixed m-auto flex w-full items-center justify-between bg-gradient-to-r from-[#111]/30 to-transparent/5 py-4 px-4 md:px-8'>
@@ -28,7 +29,8 @@ export const Navbar = () => {
         className='z-10 block h-8 w-8 xl:hidden'
         onClick={toggleNavMenu}
         aria-label='Toggle navigation menu'
-        aria-expanded={isNavbarOpen}>
+        aria-expanded={isNavbarOpen}
+      >
         {isNavbarOpen ? (
           <RxCross1 className='h-full w-full' />
         ) : (
@@ -37,29 +39,21 @@ export const Navbar = () => {
       </button>
       <nav className='hidden flex-1 justify-between xl:flex'>
         <div className='ml-12 flex items-center gap-8'>
-          {menuItems.map(item => (
+          {menuItems.map((item) => (
             <NavLink
               className={({ isActive }) =>
                 isActive
-                  ? 'border-b-2 border-b-neutral-200 font-bold'
-                  : 'border-b-2 border-b-transparent font-bold hover:border-b-neutral-200'
+                  ? "border-b-2 border-b-neutral-200 font-bold"
+                  : "border-b-2 border-b-transparent font-bold hover:border-b-neutral-200"
               }
               key={item.to}
               to={item.to}
-              aria-current='page'>
+              aria-current='page'
+            >
               {item.text}
             </NavLink>
           ))}
-          <div className='relative'>
-            <input
-              className='border-b border-b-neutral-200 bg-transparent px-4 placeholder-neutral-200 placeholder:italic'
-              type='text'
-              aria-label='Search movies and tv shows'
-              aria-placeholder='Search for Movies and TV Shows'
-              placeholder='Search title here'
-            />
-            <HiOutlineMagnifyingGlass className='absolute top-1 right-1' />
-          </div>
+          <Searchbar />
         </div>
         <div className='flex gap-2'>
           <Button label='Sign In' />
@@ -70,34 +64,27 @@ export const Navbar = () => {
       <nav
         className={
           isNavbarOpen
-            ? 'fixed top-0 right-0 flex h-screen w-full flex-col items-center gap-y-10 bg-red-500 duration-200 ease-in-out'
-            : 'fixed top-0 -right-full flex h-screen w-full flex-col items-center gap-y-10 bg-red-500 duration-200 ease-in-out'
-        }>
+            ? "fixed top-0 right-0 flex h-screen w-full flex-col items-center gap-y-10 bg-red-500 duration-200 ease-in-out"
+            : "fixed top-0 -right-full flex h-screen w-full flex-col items-center gap-y-10 bg-red-500 duration-200 ease-in-out"
+        }
+      >
         <div className='flex flex-col items-center gap-8 pt-24'>
-          {menuItems.map(item => (
+          {menuItems.map((item) => (
             <NavLink
               className={({ isActive }) =>
                 isActive
-                  ? 'border-b-2 border-b-neutral-200 font-bold'
-                  : 'border-b-2 border-b-transparent font-bold hover:border-b-neutral-200'
+                  ? "border-b-2 border-b-neutral-200 font-bold"
+                  : "border-b-2 border-b-transparent font-bold hover:border-b-neutral-200"
               }
               key={item.to}
               to={item.to}
               aria-current='page'
-              onClick={toggleNavMenu}>
+              onClick={toggleNavMenu}
+            >
               {item.text}
             </NavLink>
           ))}
-          <div className='relative'>
-            <input
-              className='border-b border-b-neutral-200 bg-transparent px-4 placeholder-neutral-200 placeholder:italic'
-              type='text'
-              aria-label='Search movies and tv shows'
-              aria-placeholder='Search for Movies and TV Shows'
-              placeholder='Search title here'
-            />
-            <HiOutlineMagnifyingGlass className='absolute top-1 right-1' />
-          </div>
+          <Searchbar />
         </div>
         <div className='flex gap-2'>
           <Button label='Sign In' />
@@ -105,5 +92,5 @@ export const Navbar = () => {
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
