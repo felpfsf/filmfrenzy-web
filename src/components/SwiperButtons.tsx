b37b5ffc0683a25922af7929280f08174595ff6f
Felipe <feferreira.sf@gmail.com>
2023-03-06 13:24:42 -0300

    style: change swiper buttons

--- src/components/SwiperButtons.tsx
+++ src/components/SwiperButtons.tsx
@@ -4,26 +4,30 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 const SwiperPrevButton = () => {
   const swiper = useSwiper();
   return (
-    <button
-      className='z-10 flex h-full w-14 items-center justify-center bg-gradient-to-r from-[#111]/95  to-transparent/5  focus:ring-2'
-      aria-labelledby='Go back to previous slide'
-      onClick={() => swiper.slidePrev()}
-    >
-      <RxCaretLeft size={48} />
-    </button>
+    <div className='z-10 flex h-full w-14 items-center justify-center bg-gradient-to-r from-[#111]/95 to-transparent/5 focus:ring-2'>
+      <button
+        className='ring-indigo-500 focus:ring-2'
+        aria-labelledby='Go back to previous slide'
+        onClick={() => swiper.slidePrev()}
+      >
+        <RxCaretLeft size={48} />
+      </button>
+    </div>
   );
 };
 
 const SwiperNextButton = () => {
   const swiper = useSwiper();
   return (
-    <button
-      className='z-10 flex h-full w-14 items-center justify-center bg-gradient-to-l from-[#111]/95  to-transparent/5 focus:ring-2'
-      aria-labelledby='Go foward to next slide'
-      onClick={() => swiper.slideNext()}
-    >
-      <RxCaretRight size={48} />
-    </button>
+    <div className='z-10 flex h-full w-14 items-center justify-center bg-gradient-to-l from-[#111]/95 to-transparent/5 focus:ring-2'>
+      <button
+        className='ring-indigo-500 focus:ring-1'
+        aria-labelledby='Go foward to next slide'
+        onClick={() => swiper.slideNext()}
+      >
+        <RxCaretRight size={48} />
+      </button>
+    </div>
   );
 };
 
