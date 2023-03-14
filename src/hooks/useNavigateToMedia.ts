fcff5360a5ca6d9135e5d9a882b67c3e9de745af
Felipe <feferreira.sf@gmail.com>
2023-03-14 17:22:55 -0300

    refactor: removed console log

--- src/hooks/useNavigateToMedia.ts
+++ src/hooks/useNavigateToMedia.ts
@@ -6,17 +6,12 @@ export const useNavigateToMedia = () => {
 
   const navigateToMedia = (title: string, id: number, type: string) => {
     const mediaType = type === "movie" ? "title" : "name";
-
-    console.log(mediaType);
-
     const slugifiedTitle = slugify(title, {
       replacement: "-",
       remove: /[^\w\s-]/,
       lower: true,
     });
-
     const path = `/${type}/${id}/${slugifiedTitle}`;
-
     nav(path);
   };
 
