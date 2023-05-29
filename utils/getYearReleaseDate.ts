cc0141dd4d8d111d0b0f4a2cb08bdd3a5f711afb
Felipe <feferreira.sf@gmail.com>
2023-05-29 19:41:28 -0300

    refactor: typeguard

--- utils/getYearReleaseDate.ts
+++ utils/getYearReleaseDate.ts
@@ -1,4 +1,4 @@
-export const getYearReleaseDate = (date: string) => {
+export const getYearReleaseDate = (date: string): number => {
   const year = new Date(date).getFullYear();
   return year;
 };
