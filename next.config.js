db8387bffe8849c6077ffcdd06f726037b814b57
Felipe <feferreira.sf@gmail.com>
2023-05-29 12:05:30 -0300

    chore: image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images:{
+    domains:["image.tmdb.org", "dummyimage.com"]
+  }
+}
 
 module.exports = nextConfig
