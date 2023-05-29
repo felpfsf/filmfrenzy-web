2d80842c00740930a60a9852699b08397515d725
Felipe <feferreira.sf@gmail.com>
2023-05-29 19:42:07 -0300

    feat: cast slider and cast card

---
+++ app/[media]/[id]/components/CastCard.tsx
@@ -0,0 +1,30 @@
+import { Credits } from "@/types/MediaModels";
+import Image from "next/image";
+
+const CastCard = ({ character, name, profile_path }: Credits) => {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <div className="w-32 h-32 overflow-hidden rounded-full">
+        <Image
+          src={
+            profile_path
+              ? `https://image.tmdb.org/t/p/original/${profile_path}`
+              : `https://dummyimage.com/2000x3000/000/fff.png&text=Foto+de+${name}`
+          }
+          alt={""}
+          width={125}
+          height={125}
+          className='h-full w-full object-cover'
+        />
+      </div>
+      <div>
+        <p className='mt-2 text-center text-sm leading-none'>{name}</p>
+        <p className='mt-2 text-center text-xs leading-none text-gray-500'>
+          {character}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CastCard;
---
+++ app/[media]/[id]/components/CastSlider.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Credits } from "@/types/MediaModels";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import CastCard from "./CastCard";
+
+interface CastSlideProps {
+  cast: Credits[];
+}
+const CastSlider = ({ cast }: CastSlideProps) => {
+  return (
+    <Swiper
+      modules={[Navigation]}
+      loop={false}
+      // navigation={true}
+      className='w-full'
+      breakpoints={{
+        320: {
+          slidesPerView: 3.2,
+          spaceBetween: 16,
+        },
+        640: {
+          slidesPerView: 4.5,
+          spaceBetween: 16,
+        },
+        1440: {
+          slidesPerView: 9.5,
+          spaceBetween: 16,
+        },
+      }}
+    >
+      <>
+        {cast.map((cast) => (
+          <SwiperSlide key={cast.id}>
+            
+            <CastCard {...cast} />
+          </SwiperSlide>
+        ))}
+      </>
+    </Swiper>
+  );
+};
+
+export default CastSlider;
