1fa626ae1cb59940273d7dba0c11b0e932bf83b3
Felipe <feferreira.sf@gmail.com>
2023-05-29 15:15:16 -0300

    feat: navigation menu items reusable

---
+++ components/NavigationMenu/index.tsx
@@ -0,0 +1,12 @@
+
+const NavigationMenu = () => {
+  return (
+    <ul className='flex gap-4 flex-col lg:flex-row'>
+      <li>Filmes</li>
+      <li>Series</li>
+      <li>Login</li>
+    </ul>
+  );
+};
+
+export default NavigationMenu
