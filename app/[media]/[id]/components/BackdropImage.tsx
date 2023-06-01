3780c273acfe7dc1457aed544522152c30d39af5
Felipe <feferreira.sf@gmail.com>
2023-06-01 15:17:04 -0300

    feat: backdrop component with overlay div

---
+++ app/[media]/[id]/components/BackdropImage.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+const posterBaseUrl = process.env.NEXT_PUBLIC_POSTER_URL;
+
+const BackdropImage = ({
+  backdropPath,
+  title,
+}: {
+  backdropPath: string;
+  title: string;
+}) => {
+  const posterUrl = `${posterBaseUrl}/${backdropPath}`;
+  const dummyPosterUrl = `https://dummyimage.com/2000x3000/000/fff.png&text=Poster+de+${title}`;
+  return (
+    <>
+      <Image
+        src={backdropPath ? posterUrl : dummyPosterUrl}
+        alt={""}
+        fill
+        blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFUlEQVR42mP8dtatnoEIwDiqkL4KASH/GVugFkABAAAAAElFTkSuQmCC'
+        placeholder='blur'
+        className='absolute left-0 top-0 -z-10 object-cover opacity-20'
+      />
+      <div className='absolute inset-0 -z-10 w-full bg-gradient-poster' />
+    </>
+  );
+};
+
+export default BackdropImage;
