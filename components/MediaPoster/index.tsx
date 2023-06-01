df9a843328d79dc571df2ed413506ca26d1ed6bf
Felipe <feferreira.sf@gmail.com>
2023-06-01 15:17:43 -0300

    refactor: imports order

--- components/MediaPoster/index.tsx
+++ components/MediaPoster/index.tsx
@@ -1,4 +1,5 @@
 import LazyImage from "../LazyImage";
+const posterUrl = process.env.NEXT_PUBLIC_POSTER_URL;
 
 interface PosterProps {
   posterPath: string;
@@ -6,7 +7,6 @@ interface PosterProps {
 }
 
 const MediaPoster = ({ posterPath, title }: PosterProps) => {
-  const posterUrl = process.env.NEXT_PUBLIC_POSTER_URL;
   const dummyPosterUrl =
     "https://dummyimage.com/2000x3000/000/fff.png&text=Poster+de+";
   return (
