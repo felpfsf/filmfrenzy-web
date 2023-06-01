fbba3bc611bbe4f208f6ffd276bee0d0a4c1feb8
Felipe <feferreira.sf@gmail.com>
2023-06-01 17:34:11 -0300

    refactor: added classname props for reusability

--- components/MediaPoster/index.tsx
+++ components/MediaPoster/index.tsx
@@ -1,16 +1,19 @@
+import clsx from "clsx";
 import LazyImage from "../LazyImage";
 const posterUrl = process.env.NEXT_PUBLIC_POSTER_URL;
 
 interface PosterProps {
   posterPath: string;
   title: string;
+  className?: string;
 }
 
-const MediaPoster = ({ posterPath, title }: PosterProps) => {
+const MediaPoster = ({ className, posterPath, title }: PosterProps) => {
+  const style = clsx("w-full min-w-[10rem] self-center overflow-hidden rounded-xl", className);
   const dummyPosterUrl =
     "https://dummyimage.com/2000x3000/000/fff.png&text=Poster+de+";
   return (
-    <div className='w-full min-w-[10rem] max-w-sm self-center overflow-hidden rounded-xl'>
+    <div className={style}>
       <LazyImage
         image={{
           alt: `Poster de ${title}`,
