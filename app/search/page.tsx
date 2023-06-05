c7548fddef068c2460e02aaaa9ed64d70b3901b0
Felipe <feferreira.sf@gmail.com>
2023-06-05 13:14:27 -0300

    style: displaying the search results

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,21 +8,17 @@ const Search = async ({
 }) => {
   const data = await fetchSearch(q);
   return (
-    <main className='mx-auto w-full max-w-screen-xl px-4 py-24'>
+    <main className='mx-auto flex w-full max-w-screen-xl flex-col gap-8 px-4 pt-24'>
       <p>
         Resultados para{" "}
         <em className='underline underline-offset-2'>&quot;{q}&quot;</em>
       </p>
-      <section>
-        <h1 className='my-6 text-left text-2xl font-semibold capitalize'>
-          Filmes
-        </h1>
+      <section className='container mx-auto flex flex-col gap-6'>
+        <h1 className='text-left text-2xl font-semibold capitalize'>Filmes</h1>
         {data?.moviesResult && <MediaSlider media={data.moviesResult} />}
       </section>
-      <section>
-        <h1 className='my-6 text-left text-2xl font-semibold capitalize'>
-          Séries
-        </h1>
+      <section className='container mx-auto flex flex-col gap-6'>
+        <h1 className='text-left text-2xl font-semibold capitalize'>Séries</h1>
         {data?.tvsResult && <MediaSlider media={data.tvsResult} />}
       </section>
     </main>
