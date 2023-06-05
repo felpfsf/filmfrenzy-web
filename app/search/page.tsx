569151acd680866ccc42b369075c02d33f9311a2
Felipe <feferreira.sf@gmail.com>
2023-06-05 12:15:37 -0300

    feat: fetch search results logic separted

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,40 +1,5 @@
 import MediaSlider from "@/components/MediaSlider";
-import { MediaDetails } from "@/types/MediaModels";
-
-const fetchSearch = async (query: string) => {
-  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const language = "pt-BR";
-  const pageCount = 1;
-
-  const REVALIDATE_TIME = 60 * 60 * 1; //1 hora
-  const SHUFFLE_THRESHOLD = 0.5;
-  const RANDOM_HERO_MEDIA_COUNT = 20;
-  // /search/multi?
-
-  const searchUrl = `${baseUrl}/search/multi?api_key=${apiKey}&language=${language}&page=${pageCount}&include_adult=false&query=${query}`;
-
-  try {
-    const searchedMediaResponse = await fetch(searchUrl, {
-      next: { revalidate: REVALIDATE_TIME },
-    });
-    if (!searchedMediaResponse.ok) {
-      throw new Error("Falha ao carregar os dados");
-    }
-
-    const searchResult: MediaDetails[] = (await searchedMediaResponse.json())
-      .results;
-
-    const moviesResult = searchResult.filter(
-      (item) => item.media_type === "movie"
-    );
-    const tvsResult = searchResult.filter((item) => item.media_type === "tv");
-
-    return { moviesResult, tvsResult };
-  } catch (error) {
-    console.error(error);
-  }
-};
+import { fetchSearch } from "@/utils/fetchSearchedMedia";
 
 const Search = async ({
   searchParams: { q },
---
+++ utils/fetchSearchedMedia.ts
@@ -0,0 +1,34 @@
+import { MediaDetails } from "@/types/MediaModels";
+
+export const fetchSearch = async (query: string) => {
+  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const language = "pt-BR";
+  const pageCount = 1;
+
+  const REVALIDATE_TIME = 60 * 60 * 1; //1 hora
+  // /search/multi?
+
+  const searchUrl = `${baseUrl}/search/multi?api_key=${apiKey}&language=${language}&page=${pageCount}&include_adult=false&query=${query}`;
+
+  try {
+    const searchedMediaResponse = await fetch(searchUrl, {
+      next: { revalidate: REVALIDATE_TIME },
+    });
+    if (!searchedMediaResponse.ok) {
+      throw new Error("Falha ao carregar os dados");
+    }
+
+    const searchResult: MediaDetails[] = (await searchedMediaResponse.json())
+      .results;
+
+    const moviesResult = searchResult.filter(
+      (item) => item.media_type === "movie"
+    );
+    const tvsResult = searchResult.filter((item) => item.media_type === "tv");
+
+    return { moviesResult, tvsResult };
+  } catch (error) {
+    console.error(error);
+  }
+};
