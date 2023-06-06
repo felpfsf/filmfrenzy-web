b17863de9e550721db7fe74cc42f59f71d548ad9
Felipe <feferreira.sf@gmail.com>
2023-06-06 16:24:19 -0300

    feat: pages for movies and tv show

--- app/[media]/page.tsx
+++ app/[media]/page.tsx
@@ -1,6 +1,18 @@
-const Media = () => {
+import Hero from "@/components/Hero";
+import { getMedia } from "@/utils/getMedia";
+
+const Media = async ({ params: { media } }: { params: { media: string } }) => {
+  console.log(media);
+  const data = await getMedia();
+  const popMovies = data?.popularMovies;
+  const popTv = data?.popularShows;
   return (
-    <main className='mx-auto w-full max-w-screen-xl px-4 py-24'>Media</main>
+    <main>
+      <section>
+        {media === "movie" && popMovies && <Hero randomHeroMedia={popMovies} />}
+        {media === "tv" && popTv && <Hero randomHeroMedia={popTv} />}
+      </section>
+    </main>
   );
 };
 
