bdff085c053bdbe20be2dfd09df43e1b0925a26e
Felipe <feferreira.sf@gmail.com>
2023-06-06 16:24:40 -0300

    refactor: clean up the fetch request of media

--- utils/getMedia.ts
+++ utils/getMedia.ts
@@ -1,48 +1,140 @@
 import { MediaDetails } from "@/types/MediaModels";
 
-interface MediaRespose {
-  results: MediaDetails[];
-}
+const fetchJSON = async (url: RequestInfo | URL) => {
+  const response = await fetch(url, { next: { revalidate: 60 * 60 * 1 } });
+  if (!response.ok) {
+    throw new Error("Falha ao carregar dados");
+  }
+  return response.json();
+};
+
+const fetchPopularMovies = async (
+  apiKey: string,
+  baseUrl: string,
+  language: string,
+  pageCount: number
+) => {
+  const url = `${baseUrl}/movie/popular?api_key=${apiKey}&page=${pageCount}&language=${language}`;
+  return fetchJSON(url);
+};
+const fetchTrendingMovies = async (
+  apiKey: string,
+  baseUrl: string,
+  language: string,
+  pageCount: number
+) => {
+  const url = `${baseUrl}/trending/movie/week?api_key=${apiKey}&page=${pageCount}&language=${language}`;
+  return fetchJSON(url);
+};
+const fetchTopRatedMovies = async (
+  apiKey: string,
+  baseUrl: string,
+  language: string,
+  pageCount: number
+) => {
+  const url = `${baseUrl}/movie/top_rated?api_key=${apiKey}&page=${pageCount}&language=${language}`;
+  return fetchJSON(url);
+};
+const fetchUpcommingMovies = async (
+  apiKey: string,
+  baseUrl: string,
+  language: string,
+  pageCount: number
+) => {
+  const url = `${baseUrl}/movie/upcoming?api_key=${apiKey}&page=${pageCount}&language=${language}`;
+  return fetchJSON(url);
+};
+
+const fetchPopularShows = async (
+  apiKey: string,
+  baseUrl: string,
+  language: string,
+  pageCount: number
+) => {
+  const url = `${baseUrl}/tv/popular?api_key=${apiKey}&page=${pageCount}&language=${language}`;
+  return fetchJSON(url);
+};
+const fetchTrendingShows = async (
+  apiKey: string,
+  baseUrl: string,
+  language: string,
+  pageCount: number
+) => {
+  const url = `${baseUrl}/trending/tv/week?api_key=${apiKey}&page=${pageCount}&language=${language}`;
+  return fetchJSON(url);
+};
+const fetchTopRatedShows = async (
+  apiKey: string,
+  baseUrl: string,
+  language: string,
+  pageCount: number
+) => {
+  const url = `${baseUrl}/tv/top_rated?api_key=${apiKey}&page=${pageCount}&language=${language}`;
+  return fetchJSON(url);
+};
 
 export const getMedia = async () => {
   const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const language = "pt-BR";
   const pageCount = 1;
+  const RANDOM_MEDIA_COUNT = 20;
 
-  const REVALIDATE_TIME = 60 * 60 * 1; //1 hora
-  const SHUFFLE_THRESHOLD = 0.5;
-  const RANDOM_HERO_MEDIA_COUNT = 20;
-
-  const moviesPopUrl = `${baseUrl}/movie/popular?api_key=${apiKey}&page=${pageCount}&language=${language}`;
-  const tvPopUrl = `${baseUrl}/tv/popular?api_key=${apiKey}&page=${pageCount}&language=${language}`;
+  if (!apiKey) {
+    throw new Error("Chave de API necessária");
+  }
+  if (!baseUrl) {
+    throw new Error("URL da API não está especificada");
+  }
 
   try {
-    const [moviePopularResponse, tvPopularResponse] = await Promise.all([
-      fetch(moviesPopUrl, { next: { revalidate: REVALIDATE_TIME } }),
-      fetch(tvPopUrl, { next: { revalidate: REVALIDATE_TIME } }),
+    const [
+      moviePopularResponse,
+      moviesTrendingResponse,
+      moviesTopResponse,
+      moviesUpcommingResponse,
+      tvPopularResponse,
+      tvTrendingResponse,
+      tvTopReponse,
+    ] = await Promise.all([
+      fetchPopularMovies(apiKey, baseUrl, language, pageCount),
+      fetchTrendingMovies(apiKey, baseUrl, language, pageCount),
+      fetchTopRatedMovies(apiKey, baseUrl, language, pageCount),
+      fetchUpcommingMovies(apiKey, baseUrl, language, pageCount),
+      fetchPopularShows(apiKey, baseUrl, language, pageCount),
+      fetchTrendingShows(apiKey, baseUrl, language, pageCount),
+      fetchTopRatedShows(apiKey, baseUrl, language, pageCount),
     ]);
 
-    if (!moviePopularResponse.ok || !tvPopularResponse.ok) {
-      throw new Error("Falha ao carregar dados");
-    }
-
-    const popularMovies: MediaDetails[] = (await moviePopularResponse.json())
-      .results;
-    const popularShows: MediaDetails[] = (await tvPopularResponse.json())
-      .results;
+    const popularMovies: MediaDetails[] = moviePopularResponse.results;
+    const trendingMovies: MediaDetails[] = moviesTrendingResponse.results;
+    const topMovies: MediaDetails[] = moviesTopResponse.results;
+    const upcomingMovies: MediaDetails[] = moviesUpcommingResponse.results;
+    const popularShows: MediaDetails[] = tvPopularResponse.results;
+    const trendingShows: MediaDetails[] = tvTrendingResponse.results;
+    const topShows: MediaDetails[] = tvTopReponse.results;
 
     const shuffledMedia = [...popularMovies, ...popularShows]
       .filter((media) => media.backdrop_path)
       .slice()
-      .sort(() => SHUFFLE_THRESHOLD - Math.random());
+      .sort(() => 0.5 - Math.random());
     const randomHeroMedia: MediaDetails[] = shuffledMedia.slice(
       0,
-      RANDOM_HERO_MEDIA_COUNT
+      RANDOM_MEDIA_COUNT
     );
 
-    return { popularMovies, popularShows, randomHeroMedia };
+    return {
+      popularMovies,
+      trendingMovies,
+      topMovies,
+      upcomingMovies,
+      popularShows,
+      trendingShows,
+      topShows,
+      randomHeroMedia,
+    };
   } catch (error) {
     console.error(error);
+    throw new Error("Falha ao carregar dados");
   }
 };
