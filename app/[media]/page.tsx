25481e9c86b0080c50ac85bd8f0854f680fa3202
Felipe <feferreira.sf@gmail.com>
2023-06-06 17:30:50 -0300

    feat: added media sections with categories

--- app/[media]/page.tsx
+++ app/[media]/page.tsx
@@ -1,16 +1,64 @@
 import Hero from "@/components/Hero";
+import MediaSlider from "@/components/MediaSlider";
 import { getMedia } from "@/utils/getMedia";
 
 const Media = async ({ params: { media } }: { params: { media: string } }) => {
-  console.log(media);
   const data = await getMedia();
-  const popMovies = data?.popularMovies;
-  const popTv = data?.popularShows;
+  const {
+    onTheAirShows,
+    popularMovies,
+    popularShows,
+    topMovies,
+    topShows,
+    trendingMovies,
+    trendingShows,
+    upcomingMovies,
+  } = data;
   return (
     <main>
       <section>
-        {media === "movie" && popMovies && <Hero randomHeroMedia={popMovies} />}
-        {media === "tv" && popTv && <Hero randomHeroMedia={popTv} />}
+        {media === "movie" && popularMovies && (
+          <Hero randomHeroMedia={popularMovies} />
+        )}
+        {media === "tv" && popularShows && (
+          <Hero randomHeroMedia={popularShows} />
+        )}
+      </section>
+
+      <section className='mb-16 pl-8'>
+        <div className='container mx-auto'>
+          <h2 className='my-6 text-left text-2xl font-semibold capitalize'>
+            Melhores avaliados:
+          </h2>
+        </div>
+        {media === "movie" && topMovies && <MediaSlider media={topMovies} />}
+        {media === "tv" && topShows && <MediaSlider media={topShows} />}
+      </section>
+
+      <section className='mb-16 pl-8'>
+        <div className='container mx-auto'>
+          <h2 className='my-6 text-left text-2xl font-semibold capitalize'>
+            Em Alta:
+          </h2>
+        </div>
+        {media === "movie" && trendingMovies && (
+          <MediaSlider media={trendingMovies} />
+        )}
+        {media === "tv" && trendingShows && (
+          <MediaSlider media={trendingShows} />
+        )}
+      </section>
+
+      <section className='mb-16 pl-8'>
+        <div className='container mx-auto'>
+          <h2 className='my-6 text-left text-2xl font-semibold capitalize'>
+            {media === "movie" ? "Em breve:" : "Séries No ar:"}
+          </h2>
+        </div>
+        {media === "movie" && upcomingMovies && (
+          <MediaSlider media={upcomingMovies} />
+        )}
+        {media === "tv" && onTheAirShows && <MediaSlider media={onTheAirShows} />}
       </section>
     </main>
   );
