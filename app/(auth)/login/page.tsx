a3f31691dbcdececf6c38cc1c67ffab8c7b2169d
Felipe <feferreira.sf@gmail.com>
2023-06-08 18:56:31 -0300

    feat: login and register pages blocked

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,55 @@
+"use client";
+import { Input } from "@/components/UI/input-form";
+import { Button } from "@/components/UI/submit-button";
+import SocialLogin from "@/components/social-login/social-login";
+import { LoginInputProps, LoginSchema } from "@/utils/validations/auth-schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+
 const Login = () => {
+  const [authError, setAuthError] = useState<string>("");
+  const methods = useForm<LoginInputProps>({
+    resolver: zodResolver(LoginSchema),
+  });
+  const onSubmitUser = async (data: LoginInputProps) => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("sending data => ", data);
+  };
   return (
-    <main className='mx-auto w-full max-w-screen-xl px-4 py-24'>Login</main>
+    <div className='w-full rounded-lg py-6 lg:bg-body/60 lg:backdrop-blur-sm'>
+      <FormProvider {...methods}>
+        <form
+          className='container flex flex-col'
+          autoComplete='off'
+          onSubmit={methods.handleSubmit(onSubmitUser)}
+        >
+          <h1 className='mb-8 pl-2 text-left text-3xl font-bold'>Entrar</h1>
+          <Input label={"Digite seu e-mail"} name={"email"} type={"email"} />
+          <Input
+            label={"Digite sua senha"}
+            name={"password"}
+            type={"password"}
+          />
+          <Button
+            text={"Entrar"}
+            error={authError}
+            loading={methods.formState.isSubmitting}
+          />
+        </form>
+      </FormProvider>
+      <p className='container mt-8 text-right text-sm'>
+        Novo por aqui?{" "}
+        <Link
+          href='/register'
+          className='font-semibold text-accent underline underline-offset-2 transition-colors duration-200 ease-in-out hover:text-accent-hover'
+        >
+          Registre-se
+        </Link>
+      </p>
+      <SocialLogin />
+    </div>
   );
 };
 
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,63 @@
+"use client";
+import { Input } from "@/components/UI/input-form";
+import { Button } from "@/components/UI/submit-button";
+import {
+  RegisterInputProps,
+  RegisterSchema,
+} from "@/utils/validations/auth-schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+
 const Register = () => {
+  const [authError, setAuthError] = useState<string>("");
+  const methods = useForm<RegisterInputProps>({
+    resolver: zodResolver(RegisterSchema),
+  });
+  const onSubmitUser = async (data: RegisterInputProps) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("sending data => ", data);
+    methods.reset()
+  };
   return (
-    <main className='mx-auto w-full max-w-screen-xl px-4 py-24'>Register</main>
+    <div className='w-full rounded-lg py-6 lg:bg-body/60 lg:backdrop-blur-sm'>
+      <FormProvider {...methods}>
+        <form
+          className='container flex flex-col'
+          autoComplete='off'
+          onSubmit={methods.handleSubmit(onSubmitUser)}
+        >
+          <h1 className='mb-8 pl-2 text-left text-3xl font-bold'>Registrar</h1>
+          <Input label={"Digite seu nome"} name={"name"} type={"text"} />
+          <Input label={"Digite seu e-mail"} name={"email"} type={"email"} />
+          <Input
+            label={"Digite sua senha"}
+            name={"password"}
+            type={"password"}
+          />
+          <Input
+            label={"Confirme sua senha"}
+            name={"passwordConfirmation"}
+            type={"password"}
+          />
+          <Button
+            text={"Entrar"}
+            error={authError}
+            loading={methods.formState.isSubmitting}
+          />
+        </form>
+      </FormProvider>
+      <p className='container mt-8 text-right text-sm'>
+        Já tem uma conta?{" "}
+        <Link
+          href='/login'
+          className='font-semibold text-accent underline underline-offset-2 transition-colors duration-200 ease-in-out hover:text-accent-hover'
+        >
+          Entre aqui
+        </Link>
+      </p>
+    </div>
   );
 };
 
