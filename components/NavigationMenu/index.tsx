651ae36fcce81b3f308cf3e6d47c19f711f463f9
Felipe <feferreira.sf@gmail.com>
2023-06-08 20:28:34 -0300

    feat: condition to display user name when logged

--- components/NavigationMenu/index.tsx
+++ components/NavigationMenu/index.tsx
@@ -1,9 +1,13 @@
 "use client";
+import { LogOut } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NavigationMenu = () => {
+  const { data: session } = useSession();
   const pathname = usePathname();
+
   return (
     <ul className='flex flex-col gap-4 lg:flex-row'>
       <li>
@@ -30,18 +34,27 @@ const NavigationMenu = () => {
           Series
         </Link>
       </li>
-      <li>
-        <Link
-          href={"/login"}
-          className={`border-b-2 font-bold hover:border-b-accent hover:text-accent-hover ${
-            pathname === "/login"
-              ? "border-b-2 border-b-accent font-bold text-accent-hover"
-              : "border-b-transparent "
-          }`}
-        >
-          Login
-        </Link>
-      </li>
+      {session && session?.user ? (
+        <>
+          {session.user.name}
+          <button onClick={() => signOut()}>
+            <LogOut />
+          </button>
+        </>
+      ) : (
+        <li>
+          <Link
+            href={"/login"}
+            className={`border-b-2 font-bold hover:border-b-accent hover:text-accent-hover ${
+              pathname === "/login"
+                ? "border-b-2 border-b-accent font-bold text-accent-hover"
+                : "border-b-transparent "
+            }`}
+          >
+            Login
+          </Link>
+        </li>
+      )}
     </ul>
   );
 };
