f75ceceef0cbc7bb8f014744eafa94be2d134df3
Felipe <feferreira.sf@gmail.com>
2023-06-10 17:43:45 -0300

    fix: corrected base url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+
 export const api = axios.create({
-  baseURL:"http://localhost:3000/api"
+  baseURL:`${BASE_URL}/api`
 })
