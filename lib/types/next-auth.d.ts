30b487d688376336f398ff9785e668fa05c7bbab
Felipe <feferreira.sf@gmail.com>
2023-06-10 17:31:53 -0300

    feat: next auth default types

---
+++ lib/types/next-auth.d.ts
@@ -0,0 +1,10 @@
+import "next-auth";
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
