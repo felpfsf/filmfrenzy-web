0fcac964b9ed64cb960fd28bdf5919cc318a9b33
Felipe <feferreira.sf@gmail.com>
2023-06-10 21:32:55 -0300

    fix: error not showing issue

--- components/auth-form/login-auth-form.tsx
+++ components/auth-form/login-auth-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { LoginInputProps, LoginSchema } from "@/lib/validations/auth-schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
@@ -9,16 +9,11 @@ import { Input } from "../UI/input-form";
 import { Button } from "../UI/submit-button";
 
 const LoginAuthForm = () => {
-  const { data: session } = useSession();
   const router = useRouter();
   const [authError, setAuthError] = useState<string>("");
   const methods = useForm<LoginInputProps>({
     resolver: zodResolver(LoginSchema),
   });
-
-  if (session) {
-    router.replace("/");
-  }
 
   const onSubmitUser = async (data: LoginInputProps) => {
     try {
@@ -28,12 +23,13 @@ const LoginAuthForm = () => {
         ...data,
       });
 
-      if (res?.ok) {
-        router.push("/");
-      } else {
+      if (res?.error) {
         console.error(res?.error);
-        setAuthError(res?.error as string);
+        setAuthError(res?.error);
+      } else {
+        router.replace("/");
       }
+
     } catch (error) {
       console.error(error);
       setAuthError(error as string);
